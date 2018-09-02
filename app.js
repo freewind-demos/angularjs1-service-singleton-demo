02894aec1555ecc0864a3512a0a87e14e6c05781
freewind <nowindlee@gmail.com>
2018-09-02 13:50:48 +0800

    demo

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const app = angular.module('app', [])
 
-app.factory('randomNumber', function () {
-    return function () {
-        return Math.random()
+app.service('hello', function () {
+    this.serviceName = 'hello-service'
+    this.hello = function (name) {
+        alert('Hello, ' + name + '! (from ' + this.serviceName + ')')
     }
 })
 
-app.controller('MyController', function ($scope, randomNumber) {
-    $scope.message = 'Random number: ' + randomNumber()
+app.controller('MyController', function ($scope, hello) {
+    $scope.userName = 'AngularJS'
+    $scope.sayHello = function () {
+        hello.hello($scope.userName)
+    }
 })
